6064f40a3e9b22550965ab8a6c05a1dbb7f37716
alihds <alih.dev@outlook.com>
2023-04-29 23:00:56 +0300

    Initial commit

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+# Arithmetic Operators
+print(" Addition: +\n", "Subtraction: -\n", "Multiplication: *\n", "Division: /\n", "Exponent: **")
+
+# Addition
+print(1 + 2)
+
+# Subtraction
+print(10 - 5)
+
+# Multiplication
+print(2 * 5)
+
+# Division
+print(8 / 2)
+
+# Exponent
+print(4 ** 3)
