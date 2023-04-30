2bfeeb7c02dd24a5edeaea9a160e229f093bee30
alihds <alih.dev@outlook.com>
2023-04-30 12:45:52 +0300

    Initial commit

---
+++ console_calculator.py
@@ -0,0 +1,23 @@
+print("First number:")
+first_number = float(input())
+print()
+print("Operator:")
+operation = str(input())
+print()
+print("Second number:")
+second_number = float(input())
+print()
+if operation == "+":
+    result = first_number + second_number
+elif operation == "-":
+    result = first_number - second_number
+elif operation == "*":
+    result = first_number * second_number
+elif operation == "/":
+    result = first_number / second_number
+elif operation == "**":
+    result = first_number ** second_number
+else:
+    print("Error")
+print("Result:")
+print(result)
