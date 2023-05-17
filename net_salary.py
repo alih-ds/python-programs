1b1366d440fbd5f981e23b08d6d86aaed61fe70d
Ali Hameed <106717840+alih-ds@users.noreply.github.com>
2023-05-17 17:43:32 +0300

    Add files via upload

---
+++ net_salary.py
@@ -0,0 +1,5 @@
+# Net Salary Calculator (After-Tax)
+total_salary = int(input("Enter your total salary: "))
+tax_percent = int(input("Enter your salary tax rate as a number: "))
+tax_conversion = tax_percent / 100
+net_salary = print("Your net salary is:", total_salary - (total_salary * tax_conversion))
